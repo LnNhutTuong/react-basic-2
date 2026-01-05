f59bab3b8fc24223152b94cf797cb3ada6d1b26b
tuong <you@example.com>
2026-01-05 10:40:37 +0700

    chang navlink to Link from react-rouder-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import "./App.scss";
 import Header from "./components/Header/Header";
+import { Link } from "react-router-dom";
+
 const App = () => {
   return (
     <div className="app-container">
       <Header />
+      <div>
+        testlink
+        <div>
+          <button className="btn">
+            <Link to="/Admin">Admin page</Link>
+          </button>
+          <button className="btn">
+            <Link to="/User">User page</Link>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,18 +2,25 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import { Link } from "react-router-dom";
 
 const BasicExample = () => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">XimenT</Navbar.Brand>
+        <Navbar.Brand href="home">XimenT</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#user">User</Nav.Link>
-            <Nav.Link href="#admin">Admin</Nav.Link>
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+            <Link to="Admin" className="nav-link">
+              Admin
+            </Link>
+            <Link to="User" className="nav-link">
+              User
+            </Link>
           </Nav>
           <Nav>
             <NavDropdown title="Settings" id="basic-nav-dropdown">
